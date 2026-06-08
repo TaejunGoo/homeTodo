0313903baf20ddf82c8c788fe0ad3ecac3578421
TaejunGoo <48707344+TaejunGoo@users.noreply.github.com>
2026-06-08 22:10:30 +0900

    Start adapting Expo tabs for homeTodo

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">homeTodo</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">함께 관리하는 집안일 TODO</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
